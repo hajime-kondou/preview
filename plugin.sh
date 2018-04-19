b495aeb1fafdef1ddc0450f4d00c28a375d773ff
Hajime Kondou <hajime.kondou@ntt.com>
2018-04-19 11:29:49 +0000

    debug the moromoro

--- plugin.sh
+++ plugin.sh
@@ -1,12 +1,14 @@
-function install_slogging {
-    PWD=$(pwd)
-    cd $DEST/slogging
-    sudo python setup.py install
-    cd ${PWD}
-}
+# plugin.sh - Devstack plugin.sh script to install and configure slogging with swift settings
+
+# Save trace setting
+XTRACE=$(set +o | grep xtrace)
+set -o xtrace
+
+echo_summary "slogging's plugin.sh was called..."
+source $DEST/slogging/devstack/lib/slogging
 
 # check for service enabled
-if is_service_enabled slogging; then
+if is_plugin_enabled slogging; then
 
     if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
         # Set up system services                  # apt-get install, pip insall
@@ -21,13 +23,18 @@ if is_service_enabled slogging; then
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
-        #echo_summary "Configuring Template"  # memcached, rsyslog, ... etc
-        #configure_template
-        :
+        echo_summary "Configuring Slogging"  # memcached, rsyslog, ... etc
+        configure_slogging
 
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
-        # Initialize and start the template service
-        :
+        # Initialize and start the
+        echo_summary "Verifying Swift installed normally"
+        verify_swift
+
+    elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
+        # Unit & Function test for slogging to get swift log
+        echo_summary "Testing Swift for Slogging"
+        test-config_slogging
     fi
 
     if [[ "$1" == "unstack" ]]; then
@@ -43,4 +50,7 @@ if is_service_enabled slogging; then
         :
     fi
 fi
+
+# Restore
+$XTRACE
 
